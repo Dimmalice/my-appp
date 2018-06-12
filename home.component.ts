87af04d92f118ee6d7fa920138bb08a9e354df21
Dimmalice <40197304+Dimmalice@users.noreply.github.com>
2018-06-12 16:53:58 +0300

    Add files via upload

---
+++ home.component.ts
@@ -0,0 +1,22 @@
+import {  NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppComponent} from './app.component';
+import {CarPartsComponent} from './car-parts.component';
+
+
+
+
+@NgModule ({
+
+    declarations: [AppComponent, CarPartsComponent],
+    imports: [BrowserModule],
+    bootstrap: [ AppComponent ],
+   
+})
+
+class AppModule{}
+
+platformBrowserDynamic()
+.bootstrapModule (AppModule);
+  
---
+++ mocks.ts
@@ -0,0 +1,23 @@
+import { CarPart } from './car-part';
+
+export const CARPARTS: CarPart[] = [{
+    "id": 1,        
+    name: "Super Tires",
+    description: "These tires are the best",
+    inStock: 5,
+    price: 5
+},
+{
+    id:2,
+    name: "Reinforced Shocks",
+    description: "Shocks made from the best materials",
+    inStock: 4,
+    price: 3
+},
+{
+    id:3,
+    name: "Exhaust",
+    description: "The best exhaust",
+    inStock: 0,
+    "price":2
+}];
